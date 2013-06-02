07562325bda70c36cc5814c800b648eec2fabb42
Mark Hallett <git@markhallett.co.uk>
2013-06-02 15:39:42 +0100

    e001, to be sped up

---
+++ euler001.py
@@ -0,0 +1,27 @@
+# euler001.py
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+def e1(n):
+    rtn = 0
+    for r in range(n):
+       print 'r',r
+       if r%3 == 0:
+           print r
+           rtn += r
+       elif r%5 == 0:
+           print r
+           rtn += r
+    return rtn
+    
+# -----------------
+
+def test():
+    rtn = e1(10)
+    print rtn
+    rtn = e1(1000)
+    print rtn
+test()
+
